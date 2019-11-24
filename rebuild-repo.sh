b20e127020942089eff86b9fe84c5aed7c489ad8
Ivan Semkin <ivan@semkin.ru>
2019-11-24 04:11:59 +0300

    rebuild-repo: Fix not refreshing pacman DB properly

--- rebuild-repo.sh
+++ rebuild-repo.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 # TODO add --clean option
 set -e
 sudo test true
 
 PACKAGES=$(cat projects.list)
+ARCH=x86_64 # TODO: Arch Selection
 
 cd ..
 
 for package in $PACKAGES; do
     guzuta omakase build ${package}
+    arch-nspawn ./chroot-${ARCH}/root pacman -Syy
 done
