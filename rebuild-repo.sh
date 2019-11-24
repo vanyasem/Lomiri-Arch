5f08854a5a67da82cae0580e16f710901c910a2f
Ivan Semkin <ivan@semkin.ru>
2019-11-24 04:35:34 +0300

    rebuild-repo: Update the chroot prior to building

--- rebuild-repo.sh
+++ rebuild-repo.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 # TODO add --clean option
 set -e
 sudo test true
@@ -7,6 +7,7 @@ PACKAGES=$(cat projects.list)
 ARCH=x86_64 # TODO: Arch Selection
 
 cd ..
+arch-nspawn ./chroot-${ARCH}/root pacman -Syyu
 
 for package in $PACKAGES; do
     guzuta omakase build ${package}
